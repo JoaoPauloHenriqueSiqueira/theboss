c8de3d0a0e941a84ad123c1dd48fecbe4e6f39be
João <joao.henriquesiqueirati@gmail.com>
2020-09-30 08:01:03 -0300

    Mensagem de boas vindas e recuperacao de senha redirect

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -28,7 +28,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/page-blank';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
--- resources/views/emails/active.blade.php
+++ resources/views/emails/active.blade.php
@@ -1,7 +1,7 @@
 <html>
 
 <body>
-    <p>Olá {{ $user->name }}!</p>
+    <p>{{ $user->name }}!</p>
     <p></p>
     <p>Seu código de ativação é {{ $token }}</p>
 </body>
