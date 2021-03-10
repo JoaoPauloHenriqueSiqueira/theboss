a621523770c6e3cff21b15cb2daf66d0a42d0f28
Joao <joao.henriquesiqueirati@gmail.com>
2021-03-10 10:34:59 -0300

    SSH

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -78,7 +78,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if(env('APP_ENV') !== 'local') {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
