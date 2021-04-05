a43839f394982fa53a9b437a71a16b9544eb7d0c
unknown <joao.henriquesiqueirati@gmail.com>
2021-04-05 17:19:39 -0300

    FOrce schema removido

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -77,8 +77,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('APP_ENV') !== 'local') {
-            $url->forceScheme('https');
-        }
+        // if(env('APP_ENV') !== 'local') {
+        //     $url->forceScheme('https');
+        // }
     }
 }
